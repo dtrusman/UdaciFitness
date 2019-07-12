682c70e52f77a3c19ea4c3aac7031b9c1a6c0b9b
Daniel Trusman <daniel.trusman@kaiostech.com>
2019-07-12 15:18:55 -0400

    First commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import { History } from './components'
+
+export default function App() {
+    return (
+        <Provider store={createStore(reducer)}>
+            <View style={{ flex: 1 }}>
+                <History />
+            </View>
+        </Provider>
+    );
+}
+
---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as AddEntry } from './AddEntry';
+export { default as DateHeader } from './DateHeader';
+export { default as UdaciSlider } from './UdaciSlider';
+export { default as UdaciSteppers } from './UdaciSteppers';
+export { default as TextButton } from './TextButton';
+export { default as History } from './History';
+export { default as MetricCard } from './MetricCard';
